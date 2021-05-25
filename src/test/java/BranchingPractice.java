bed78f1374c645a4bba2ffbe2ac7988ed4dc0e30
m-abdurahman <miya_abdurahman@yahoo.com>
2021-05-25 19:23:44 -0400

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,6 +1,13 @@
 public class BranchingPractice {
 
     public static void main(String[] args) {
+// things we've done so far
+        //created new project
+        //enable VCS to Git
+        //added .gitignore with .idea & target folder & .iml file
+        //made initial commit by keeping track of all files
+        //shared project on github
+
 
     }
 }
